8b385a1821d4d5b119b4183b1afcae4911a727b4
g1nlyf <131361929+g1nlyf@users.noreply.github.com>
2026-02-05 16:00:38 +0100

    feat: Add animated components index

---
+++ frontend/src/components/animated/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Animated Components
+ * Централизованный экспорт анимированных компонентов
+ */
+
+export * from './AnimatedCard'
+export * from './AnimatedButton'
+export * from './StaggerChildren'
